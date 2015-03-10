ef36a2f2d49f16e815dea46dcfa097e2ed314a8c
Gokhan Karadas <goldenilkay92@gmail.com>
2015-03-10 13:03:07 +0200

    Add HidePoweredByHeaderMiddleware.

---
+++ Helmet.Net/HidePoweredByHeader/HidePoweredByHeaderMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Helmet.Net.HidePoweredByHeader
+{
+   public class HidePoweredByHeaderMiddleware:OwinMiddleware
+    {
+       private IHidePoweredOptions _options;
+
+
+       public HidePoweredByHeaderMiddleware(OwinMiddleware next)
+           : base(next)
+       {
+       }
+       public HidePoweredByHeaderMiddleware(OwinMiddleware next,IHidePoweredOptions options) : base(next)
+       {
+           _options = options;
+       }
+
+       public override async Task Invoke(IOwinContext context)
+       {
+           _options = _options ?? new HidePoweredOptions {SetTo =null};
+
+           if (!string.IsNullOrEmpty(_options.SetTo))
+           {
+               context.Response.Headers.Add("X-Powered-By", new[] {_options.SetTo});
+           }
+           else
+           {
+               context.Response.Headers.Remove("X-Powered-By");
+           }
+           await Next.Invoke(context);
+
+       }
+    }
+}
